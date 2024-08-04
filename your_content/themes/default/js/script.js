a7f1291c41956ac4fee5e48320e1704d8341dd0b
CosmicCaliber <natenunn20@gmail.com>
2024-08-04 02:08:12 -0500

    update to hide nav bar during

--- your_content/themes/default/js/script.js
+++ your_content/themes/default/js/script.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var linksBar = document.getElementById('links-bar');
 
     hamburgerMenu.addEventListener('click', function() {
-        if (linksBar.style.display === 'block') {
+        if (linksBar.style.display === 'flex' || linksBar.style.display === '') {
             linksBar.style.display = 'none';
         } else {
-            linksBar.style.display = 'block';
+            linksBar.style.display = 'flex';
         }
     });
 });
