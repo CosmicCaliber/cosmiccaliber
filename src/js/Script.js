83231dcda5adccdc6a744cd162021c3822678fe8
CosmicCaliber <natenunn20@gmail.com>
2024-08-03 22:52:25 -0500

    Update Script.js

--- src/js/Script.js
+++ src/js/Script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var acc = document.getElementsByClassName("accordion-button");
     var i;
 
+    // Toggle accordion content
     for (i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function() {
             this.classList.toggle("active");
@@ -13,4 +14,24 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     }
+
+    // Function to handle display based on screen size
+    function handleResize() {
+        var desktopNav = document.getElementById('links-bar');
+        var accordion = document.querySelector('.accordion');
+        if (window.innerWidth <= 768) {
+            desktopNav.style.display = 'none';
+            accordion.style.display = 'flex';
+        } else {
+            desktopNav.style.display = 'block';
+            accordion.style.display = 'none';
+        }
+    }
+
+    // Initial check
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
 });
+
